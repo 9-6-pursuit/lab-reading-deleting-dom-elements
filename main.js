c4baa55fd26e289f44b4f2124aa97f157ae8a098
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-03-15 15:02:54 -0400

    Done

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector("li").remove()
+document.querySelector("span").remove()
+document.querySelector(".posts article").remove()
+let titles = document.querySelectorAll("h3")
+titles.forEach(title => {
+    title.remove()
+});
+// document.querySelector("h3").remove()
+
+
