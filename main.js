f3a020c3cb95f6553d4765db92291fd72a635fe0
wrightunik <wrightunik23@gmail.com>
2023-03-14 22:13:05 -0400

    Finished Lab

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector("li").remove();
+
+document.querySelector("span").remove();
+
+document.querySelector(".posts article").remove();
+
+const headers = document.querySelectorAll("article h3");
+headers[0].remove() + headers[1].remove() 
