4be7e32e2a22cd79ab2a50b3108bacda49ab5fb5
Wai leong Chong <waileongchong@wais-air.mynetworksettings.com>
2023-03-14 22:13:03 -0400

    deleting dom elements lab

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("header").querySelector("li").remove();
+
+document.querySelector("span").remove();
+
+document.querySelectorAll("h3").forEach(h3 => {
+    h3.remove();
+})
+
+document.querySelector(".posts article").remove();
+
+
