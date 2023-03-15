bf9a5d91e3e02de8be0838c0f1bf209cdf9bac1b
crsvalle <cristian.valle@pursuit.org>
2023-03-14 21:51:53 -0400

    dom

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector("li").remove()
+
+document.querySelector("span").remove()
+
+document.querySelector(".posts article").remove()
+
+const x = document.querySelectorAll("h3");
+x.forEach(title => title.remove())
