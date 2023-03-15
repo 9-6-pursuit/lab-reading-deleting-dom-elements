441e457ea293946bfd0837c785f91f7a3a7a8770
salvagebar <salvagebar@gmail.com>
2023-03-15 16:26:49 -0400

    Passes all tests

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const firstNavLink = document.querySelector("nav a");
+const firstNavLink = document.querySelector("header nav ul li");
 const featuredReadTime = document.querySelector(".featured span");
 const planningArticle = document.querySelectorAll("article")[3];
 const nonfeaturedTitles = document.querySelectorAll(".posts h3");
