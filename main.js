a2ceffe2b50dfa90ea226b80e38102d2b9a626a3
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-14 22:07:14 -0400

    Cypress crashed bc of Go Live Server Extension...Trash

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector("li").remove()
+document.querySelector("span").remove()
+document.querySelector("article")[2].remove()
+
+let articles = document.querySelectorAll("h3")
+articles.forEach(article => {
+    article.remove()
+})
+
+
+
+
